531e3a02b4b678a7209875595a890808cca97e7d
Richi Rodríguez <134666251+RichiRodri11@users.noreply.github.com>
2025-10-28 16:52:11 -0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// document.addEventListener("DOMContentLoaded", function () {
+//     const birthDate = new Date(2006, 3, 11); // Mes 3 = abril
+//     const today = new Date();
+//     let age = today.getFullYear() - birthDate.getFullYear();
+//     const m = today.getMonth() - birthDate.getMonth();
+  
+//     if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+//       age--;
+//     }
+  
+//     document.getElementById("nombreEdad").textContent = `Richard Rodríguez, ${age} años`;
+//   });
+  
